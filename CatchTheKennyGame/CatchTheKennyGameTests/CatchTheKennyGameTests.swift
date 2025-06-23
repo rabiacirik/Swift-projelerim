5353c7e27a97a8e5392183377c833c5fab3c9b96
Rabia Cırık <rabiacirik89@gmail.com>
2025-06-23 15:15:49 +0300

    CatchTheKennyGame
    Swift ile yazıldı

---
+++ CatchTheKennyGame/CatchTheKennyGameTests/CatchTheKennyGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatchTheKennyGameTests.swift
+//  CatchTheKennyGameTests
+//
+//  Created by Rabia Cırık on 13.06.2025.
+//
+
+import Testing
+@testable import CatchTheKennyGame
+
+struct CatchTheKennyGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
